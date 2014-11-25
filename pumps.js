d43dcdc91b84d9d1e39aab0152896aed793d2578
Apados <mbell08426@gmail.com>
2014-11-25 07:58:22 -0500

    Update pumps.js

--- pumps.js
+++ pumps.js
@@ -11,35 +11,59 @@ to the Arduino turn on and off in a pattern.
 var webSocket = require('ws'),
 	ws = new webSocket('ws://127.0.0.1:6437'),
 	five = require('johnny-five'),
+	Leap = require('leapjs'),
 	board = new five.Board(),
 	motor, frame, hand;
 var oldHandId = 0;
+var handType;
+var oldGestureId = 0;
+var speed = 5;
+var extendedFingers = 0;
 
 //self-invoking function
 board.on('ready', function() 
 {	
-	ws.on('message', function(data, flags) 
-	{
-		//getting the frame from the Leap Motion
-		frame = JSON.parse(data); 
-		
-		checkData(frame);
+	var controller = Leap.loop({enableGestures: true}, function(frame) 
+	{ 	
+		checkData(frame, controller);
 	});
 });
 
-function checkData(frame)
+function checkData(frame, controller)
 {
-	this.frame = frame;
 	//creating a hand object
 	var hand = frame.hands[0];
 	//getting the current number of fingers present
-	var currentFingers = frame.pointables.length;
+	var finger;
 	var newHandId;
+
+	
+	
+	////////////////////////////////////////NEW CODE//////////////////////////////////////////
+	var gesture;
+	var type;
+	var duration;
+	
+	//enabling gestures
+	///////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	
 	
 	//catching undefined hand objects when no hands are present
 	if(typeof hand != "undefined")
 	{
+//		extendedFingers = 0;
 		newHandId = hand.id;
+		
+		/*for(i = 0; i < 5; i++)
+		{
+			finger = hand.fingers[i];
+			if(finger.extended) 
+			{
+				extendedFingers++;
+			}
+		}*/
 	}
 	
 	//if there is a new hand, we start the pumps in a new pattern
@@ -47,13 +71,87 @@ function checkData(frame)
 	//Arduino with input and the motors won't run
 	if(newHandId != oldHandId && typeof hand != "undefined")
 	{
-		//always start the new pattern with motorA
-		runMotorA(currentFingers);
-		//storing the hand id of the current hand
-		oldHandId = newHandId;
+		type = getHandType(frame);
+		
+		if(frame.gestures.length > 0 && type == "right")
+		{
+			gesture = frame.gestures[0];
+			
+			if(gesture.duration/1000 > 100)
+			{
+				getGestures(gesture, frame);
+				/*console.log("Sending:");
+				console.log(gesture);*/
+			}
+		}
+		
+		else if(type == "left")
+		{
+			extendedFingers = 0;
+			for(i = 0; i < 5; i++)
+			{
+				finger = hand.fingers[i];
+				if(finger.extended) 
+				{
+					extendedFingers++;
+				}
+			}
+			
+			console.log(extendedFingers);
+			//always start the new pattern with motorA
+			runMotorA(extendedFingers);
+			console.log(extendedFingers);
+			
+			//storing the hand id of the current hand
+			oldHandId = newHandId;
+		}
 	}
 }
 
+function getHandType(frame)
+{
+	//getting whether the hand is a left or right hand
+	var hand = frame.hands[0];
+	if(typeof hand != "undefined")
+	{
+		handType = hand.type;
+	}
+	return handType;
+}
+
+function getGestures(gesture, frame)
+{
+	var gestures = gesture;
+	var type;
+	var duration;
+	var newGestureId;
+	
+	console.log(gesture.type);
+	if(gesture.type == "swipe")
+	{
+
+		var isHorizontal = Math.abs(gestures.direction[0]) > Math.abs(gestures.direction[1]);
+		newGestureId = gesture.id;
+		/*console.log("Getting:");
+		console.log(gesture);*/
+	}
+	
+	//Classify as right-left or up-down
+	if( (isHorizontal) &&(oldGestureId != newGestureId) )
+	{
+	  if(gestures.direction[0] > 0)
+	  {
+		speed++;
+		console.log(speed);
+	  } 
+	  else 
+	  {
+		if(speed > 1) speed--;
+		console.log(speed);
+	  }
+	  oldGestureId = newGestureId;
+	}
+}
 /*********************************************************************
 Methods that control the pumps and the patterns they run in.
 This is messier than I would like it to be. The team needs to 
@@ -72,6 +170,7 @@ function runMotorA(num)
 	//when I made them global, javascript kept throwing
 	//a TypeError. It was saying the objects were of type null
 	var number = num;
+	var runSpeed = speed * 1000;
 	var motorA = new five.Motor([3, 0, 2]);
 	var motorB = new five.Motor([5, 0, 4]);
 	var motorC = new five.Motor([6, 0, 7]);
@@ -80,24 +179,26 @@ function runMotorA(num)
 	
 	if(number == 1)
 	{
-		
 		motorA.start(255);	
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorA.stop();
-			runMotorB(number);
+			if(extendedFingers == 1) 
+			{
+				runMotorB(number);
+			}
 		});
 	}
 	else if(number == 2)
-	{
+	{			
 		motorA.start(255);
 		motorB.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorA.stop();
 			motorB.stop();
-			runMotorC(number);
+			if(extendedFingers == 2) runMotorC(number);
 		});
 	}
 	else if(number == 3)
@@ -106,12 +207,12 @@ function runMotorA(num)
 		motorB.start(255);
 		motorC.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorA.stop();
 			motorB.stop();
 			motorC.stop();
-			runMotorD(number);
+			if(extendedFingers == 3) runMotorD(number);
 		});
 	}
 	else if(number == 4)
@@ -121,13 +222,13 @@ function runMotorA(num)
 		motorC.start(255);
 		motorD.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorA.stop();
 			motorB.stop();
 			motorC.stop();
 			motorD.stop();
-			runMotorE(number);
+			if(extendedFingers == 4) runMotorE(number);
 		});
 	}
 	else if(number == 5)
@@ -138,22 +239,22 @@ function runMotorA(num)
 		motorD.start(255);
 		motorE.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorA.stop();
 			motorB.stop();
 			motorC.stop();
 			motorD.stop();
 			motorE.stop();
-			runMotorE(number);
+			if(extendedFingers == 5) runMotorE(number);
 		});
-	}
-		
+	}	
 }
 
 function runMotorB(num)
 {
 	var number = num;
+	var runSpeed = speed * 1000;
 	var motorA = new five.Motor([3, 0, 2]);
 	var motorB = new five.Motor([5, 0, 4]);
 	var motorC = new five.Motor([6, 0, 7]);
@@ -161,13 +262,12 @@ function runMotorB(num)
 	var motorE = new five.Motor([10, 0, 12]);
 	
 	if(number == 1)
-	{
-		
+	{		
 		motorB.start(255);	
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorB.stop();
-			runMotorC(number);
+			if(extendedFingers == 1) runMotorC(number);
 		});
 	}
 	else if(number == 2)
@@ -175,11 +275,11 @@ function runMotorB(num)
 		motorB.start(255);
 		motorC.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorB.stop();
 			motorC.stop();
-			runMotorD(number);
+			if(extendedFingers == 2) runMotorD(number);
 		});
 	}
 	else if(number == 3)
@@ -188,12 +288,12 @@ function runMotorB(num)
 		motorC.start(255);
 		motorD.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorB.stop();
 			motorC.stop();
 			motorD.stop();
-			runMotorE(number);
+			if(extendedFingers == 3) runMotorE(number);
 		});
 	}
 	else if(number == 4)
@@ -203,13 +303,13 @@ function runMotorB(num)
 		motorD.start(255);
 		motorE.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorB.stop();
 			motorC.stop();
 			motorD.stop();
 			motorE.stop();
-			runMotorA(number);
+			if(extendedFingers == 4) runMotorA(number);
 		});
 	}
 }
@@ -217,6 +317,7 @@ function runMotorB(num)
 function runMotorC(num)
 {
 	var number = num;
+	var runSpeed = speed * 1000;
 	var motorA = new five.Motor([3, 0, 2]);
 	var motorB = new five.Motor([5, 0, 4]);
 	var motorC = new five.Motor([6, 0, 7]);
@@ -227,10 +328,10 @@ function runMotorC(num)
 	{
 		
 		motorC.start(255);	
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorC.stop();
-			runMotorD(number);
+			if(extendedFingers == 1) runMotorD(number);
 		});
 	}
 	else if(number == 2)
@@ -238,11 +339,11 @@ function runMotorC(num)
 		motorC.start(255);
 		motorD.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorC.stop();
 			motorD.stop();
-			runMotorE(number);
+			if(extendedFingers == 2) runMotorE(number);
 		});
 	}
 	else if(number == 3)
@@ -251,12 +352,12 @@ function runMotorC(num)
 		motorD.start(255);
 		motorE.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorC.stop();
 			motorD.stop();
 			motorE.stop();
-			runMotorA(number);
+			if(extendedFingers == 3) runMotorA(number);
 		});
 	}
 	else if(number == 4)
@@ -266,13 +367,13 @@ function runMotorC(num)
 		motorE.start(255);
 		motorA.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorC.stop();
 			motorD.stop();
 			motorE.stop();
 			motorA.stop();
-			runMotorB(number);
+			if(extendedFingers == 4) runMotorB(number);
 		});
 	}
 }
@@ -280,6 +381,7 @@ function runMotorC(num)
 function runMotorD(num)
 {
 	var number = num;
+	var runSpeed = speed * 1000;
 	var motorA = new five.Motor([3, 0, 2]);
 	var motorB = new five.Motor([5, 0, 4]);
 	var motorC = new five.Motor([6, 0, 7]);
@@ -290,10 +392,10 @@ function runMotorD(num)
 	{
 		
 		motorD.start(255);	
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorD.stop();
-			runMotorE(number);
+			if(extendedFingers == 1) runMotorE(number);
 		});
 	}
 	else if(number == 2)
@@ -301,11 +403,11 @@ function runMotorD(num)
 		motorD.start(255);
 		motorE.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorD.stop();
 			motorE.stop();
-			runMotorA(number);
+			if(extendedFingers == 2) runMotorA(number);
 		});
 	}
 	else if(number == 3)
@@ -314,12 +416,12 @@ function runMotorD(num)
 		motorE.start(255);
 		motorA.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorD.stop();
 			motorE.stop();
 			motorA.stop();
-			runMotorB(number);
+			if(extendedFingers == 3) runMotorB(number);
 		});
 	}
 	else if(number == 4)
@@ -329,13 +431,13 @@ function runMotorD(num)
 		motorA.start(255);
 		motorB.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorD.stop();
 			motorE.stop();
 			motorA.stop();
 			motorB.stop();
-			runMotorC(number);
+			if(extendedFingers == 4) runMotorC(number);
 		});
 	}
 }
@@ -343,6 +445,7 @@ function runMotorD(num)
 function runMotorE(num)
 {
 	var number = num;
+	var runSpeed = speed * 1000;
 	var motorA = new five.Motor([3, 0, 2]);
 	var motorB = new five.Motor([5, 0, 4]);
 	var motorC = new five.Motor([6, 0, 7]);
@@ -353,10 +456,10 @@ function runMotorE(num)
 	{
 		
 		motorE.start(255);	
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorE.stop();
-			runMotorA(number);
+			if(extendedFingers == 1) runMotorA(number);
 		});
 	}
 	else if(number == 2)
@@ -364,11 +467,11 @@ function runMotorE(num)
 		motorE.start(255);
 		motorA.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorE.stop();
 			motorA.stop();
-			runMotorB(number);
+			if(extendedFingers == 2) runMotorB(number);
 		});
 	}
 	else if(number == 3)
@@ -377,12 +480,12 @@ function runMotorE(num)
 		motorA.start(255);
 		motorB.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorE.stop();
 			motorA.stop();
 			motorB.stop();
-			runMotorC(number);
+			if(extendedFingers == 3) runMotorC(number);
 		});
 	}
 	else if(number == 4)
@@ -392,16 +495,16 @@ function runMotorE(num)
 		motorB.start(255);
 		motorC.start(255);
 		
-		board.wait(5000, function()
+		board.wait(runSpeed, function()
 		{
 			motorE.stop();
 			motorA.stop();
 			motorB.stop();
 			motorC.stop();
-			runMotorD(number);
+			if(extendedFingers == 4) runMotorD(number);
 		});
 	}
-	else if(number == 5)
+	else if(extendedFingers == 5)
 	{
 		runMotorA(5);
 	}
